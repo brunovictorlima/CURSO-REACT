b95e84be6751cef27d15f483173daa4dcb813f04
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2024-06-07 03:16:31 -0300

    Pegando dados de inputs
    Usando o evento onChange para alterar os estados dos inputs.

--- 5_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import "./MyForm.css";
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
+  console.log(email);
+
   return (
     <div>
       {/* 1 - criação de form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o seu nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
         </div>
         {/* 2 - label envolvendo input */}
         <label>
           <span>E-mail:</span>
-          <input type="email" name="email" placeholder="Digite o seu e-mail" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
