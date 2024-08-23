033b85aab30742f69e1d6bad37a884ff65c3f63d
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2024-08-23 13:49:34 -0300

    Configuração de Rotas Dinâmicas

--- 8_REACT_ROUTER/src/App.jsx
+++ 8_REACT_ROUTER/src/App.jsx
@@ -2,20 +2,24 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
-
-import Navbar from "./components/Navbar";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <>
       <h1>React Router</h1>
       <BrowserRouter>
+        {/* 2 - links com react router */}
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* 4 - Rota dinâmica */}
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
