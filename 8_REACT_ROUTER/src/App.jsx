85198fced8aa22aa0207b38e457cbbc685ece53c
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2024-08-24 19:30:09 -0300

    Revisar aula de SearchParams

--- 8_REACT_ROUTER/src/App.jsx
+++ 8_REACT_ROUTER/src/App.jsx
@@ -3,11 +3,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import SearchForm from "./components/SearchForm";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -16,6 +19,8 @@ function App() {
       <BrowserRouter>
         {/* 2 - links com react router */}
         <Navbar />
+        {/* 9 - search */}
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -23,6 +28,10 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/* 6 - nested route */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 9 - search */}
+          <Route path="/search" element={<Search />} />
+          {/* 7 - no match route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
